ba4ed96950fcc8464acdee1e5a658a34e582cbdf
dgenutis <dainiusgenutis@yahoo.com>
2024-10-25 16:24:17 +0300

    figures constructor

--- src/models/figures/Rock.tsx
+++ src/models/figures/Rock.tsx
@@ -1,3 +1,13 @@
-import { Figure } from "./Figure";
+import { Cell } from "../Cell";
+import { Colors } from "../Colors";
+import { Figure, FigureNames } from "./Figure";
+import blackLogo from "../../assets/black-rook.png";
+import whiteLogo from "../../assets/white-rook.png";
 
-export class Rock extends Figure {}
+export class Rock extends Figure {
+  constructor(color: Colors, cell: Cell) {
+    super(color, cell);
+    this.logo = color === Colors.BLACK ? blackLogo : whiteLogo;
+    this.name = FigureNames.ROOK;
+  }
+}
