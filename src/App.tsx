232b4e9a34c1cea531ef4319b8244c01871ee6dc
dgenutis <dainiusgenutis@yahoo.com>
2024-11-03 13:27:44 +0200

    debug

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,8 @@ import Timer from './components/Timer';
 
 const App = () => {
   const [board, setBoard] = useState(new Board())
-  const [whitePlayer, setWhitePlayer] = useState(new Player(Colors.WHITE));
-  const [blackPlayer, setBlackPlayer] = useState(new Player(Colors.BLACK))
+  const [whitePlayer, /*setWhitePlayer */] = useState(new Player(Colors.WHITE));
+  const [blackPlayer,/* setBlackPlayer */] = useState(new Player(Colors.BLACK))
   const [currentPlayer, setCurrentPlayer] = useState<Player | null>(null)
   useEffect(() => {
     restart ()
